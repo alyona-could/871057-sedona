353b9f518975370e476c867dd78345a70c73dbdf
alyona-could <44996139+alyona-could@users.noreply.github.com>
2019-01-17 12:04:45 +0300

    минификация стилей и js + аутлайн на чекбоксы повесила

---
+++ js/min.script.js
@@ -0,0 +1,1 @@
+var searchOpen=document.querySelector(".form-title");var searchForm=document.querySelector(".register-form");if(searchOpen){if(searchForm){searchForm.classList.add("register-form-hidden")};searchOpen.addEventListener("click",function(event){event.preventDefault();searchForm.classList.toggle("register-form-hidden");searchForm.classList.toggle("register-form-shown")})}
